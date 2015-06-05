4715841a1d5e361b5cf6d4ac8569ea65490cde5b
campuslifeceo <wangji0604@live.cn>
2015-06-05 10:39:57 +0800

    add time server

--- NettyStudy/src/main/java/chatroom/ChatRoomServerRecvHandler.java
+++ NettyStudy/src/main/java/chatroom/ChatRoomServerRecvHandler.java
@@ -1,6 +1,8 @@
 package chatroom;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
@@ -26,5 +28,22 @@ public class ChatRoomServerRecvHandler extends ChannelInboundHandlerAdapter{
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause){
 		cause.printStackTrace();
 		ctx.close();
+	}
+	
+	
+	@Override
+	public void channelActive(final ChannelHandlerContext ctx){
+		final ByteBuf time = ctx.alloc().buffer(4);
+		
+		time.writeInt((int) (System.currentTimeMillis()/1000L + 2208988800L));
+		
+		final ChannelFuture f = ctx.writeAndFlush(time);
+		f.addListener(new ChannelFutureListener(){
+			@Override
+			public void operationComplete(ChannelFuture future){
+				assert f == future;
+				ctx.close();
+			}
+		});
 	}
 }
